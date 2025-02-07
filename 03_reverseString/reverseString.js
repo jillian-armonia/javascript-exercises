af583b1695da61bbe95c244a48115c92e819edfb
Jillian Armonia <jillian.marice@gmail.com>
2025-02-07 16:10:44 +0900

    pass 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strArray = string.split('');
+    let result = '';
 
+    for (let i = strArray.length - 1; i >= 0; i--){
+        result += strArray[i];
+    }
+
+     return result
 };
 
 // Do not edit below this line
