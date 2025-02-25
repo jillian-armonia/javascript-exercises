aeaaba8886d92e23d62cf726c384191ad9a1482f
Jillian Armonia <jillian.marice@gmail.com>
2025-02-25 14:42:20 +0900

    finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 //UNTIL THIS TEST, then check off the JS on TOP
 
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5/9);
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9/5)) + 32;
+  return Number(result.toFixed(1))
 };
 
 // Do not edit below this line
