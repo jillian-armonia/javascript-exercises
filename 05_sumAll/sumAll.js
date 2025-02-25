f118ff0fc1a5e024049c6ff2fa41d15ccefbc26d
Jillian Armonia <jillian.marice@gmail.com>
2025-02-25 13:20:27 +0900

    finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return "ERROR";
 
+
+    if (start < end){
+        while (start <= end){
+            sum += start;
+            start++;
+        }
+    } else if (start > end){
+        while (start >= end){
+            sum += start;
+            start--;
+        }
+    }
+
+
+     return sum;
 };
 
 // Do not edit below this line
